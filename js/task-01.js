73858437a23eaa0a64b1c224a165a31b0c3c931d
Pavlo-Ivanochok <ivanochokpavlo2@gmail.com>
2024-04-07 20:54:29 +0300

    update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,5 @@
 const itemsRef = document.querySelectorAll('.item');
-console.log(`Number of categories:${itemsRef.length}`);
+console.log(`Number of categories: ${itemsRef.length}`);
 itemsRef.forEach(item => {
     const categoryName = item.querySelector("h2").textContent;
     const quantity = item.querySelectorAll("ul li").length;
