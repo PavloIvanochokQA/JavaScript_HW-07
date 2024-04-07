f6597a56f22e4f13647cd28d3465d8b2d383de8d
Pavlo-Ivanochok <ivanochokpavlo2@gmail.com>
2024-04-07 21:09:47 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,32 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 };
 
 const btnCreateRef = document.querySelector('button[data-create]');
 const btnDestroyRef = document.querySelector('button[data-destroy]');
 const divBoxes = document.querySelector('#boxes');
-const inputFieldRef = document.querySelector("input");
+const inputFieldRef = document.querySelector('input');
 
-btnCreateRef.addEventListener("click", createBoxes);
-btnDestroyRef.addEventListener("click", destroyBoxes);
+btnCreateRef.addEventListener('click', createBoxes);
+btnDestroyRef.addEventListener('click', destroyBoxes);
 
-function createBoxes(amount) {
-  amount = inputFieldRef.value;
+function createBoxes() {
+  const amount = inputFieldRef.value;
 
-  let markup = "";
+  let markup = '';
 
   for (let index = 0; index < amount; index++) {
-    let divWidth = 30 + (index * 10);
-    let divHeight = 30 + (index * 10);
-    markup = markup.concat(
-      `<div width="${divWidth}" height = "${divHeight}" style="background-color:${getRandomHexColor()}"></div>`
-    );
+    let divWidth = 30 + index * 10;
+    let divHeight = 30 + index * 10;
+    markup += `<div style="width:${divWidth}px; height:${divHeight}px; background-color:${getRandomHexColor()}"></div>`;
   };
-  divBoxes.innerHTML = markup
+  divBoxes.innerHTML = markup;
 };
 
 function destroyBoxes() {
-  divBoxes.innerHTML = ""
+  divBoxes.innerHTML = '';
 };
